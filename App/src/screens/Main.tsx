93075f3bf0e9edc7a79ff907f4ba341fe69f1fa2
NeginOlfat <neginolfat@yahoo.com>
2023-07-02 06:43:08 +0330

    initial React Native

---
+++ App/src/screens/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Main = () => {
+    return (
+        <Text>
+            Hello, this is main screen
+        </Text>
+    )
+}
+
+export default React.memo(Main);
